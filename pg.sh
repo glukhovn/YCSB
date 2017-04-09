4ee1fb6ed695ed19020c2e5d1de1d75c6afb4eeb
Nikita Glukhov <n.gluhov@postgrespro.ru>
2017-04-09 20:53:58 +0300

    Update postgres jsbc driver version

--- pg.sh
+++ pg.sh
@@ -2,4 +2,4 @@
 
 DATE=$(date +"%F_%T")
 echo $DATE
-./bin/ycsb $1 pgjsonb -s -P workloads/workload$2 -cp jdbc-binding/lib/postgresql-9.4.1207.jar -P pg.dat -threads $3 > pg_${1}_workload${2}_results_${DATE}
+./bin/ycsb $1 pgjsonb -s -P workloads/workload$2 -cp jdbc-binding/lib/postgresql-9.4.1212.jar -P pg.dat -threads $3 > pg_${1}_workload${2}_results_${DATE}
